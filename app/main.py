35e314c675bda9336a8c558ec6baaef3dbf4816f
Dhiraj Patra <dhiraj.patra@gmail.com>
2025-07-04 12:15:02 +0530

    updated

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 # app/main.py
+import os
+import psycopg2
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -15,6 +17,26 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI and Kubernetes!"}
+
+@app.get("/db-check")
+def db_check():
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT", "5432")
+        )
+        curr = conn.cursor()
+        # Execute a simple query to check the connection
+        curr.execute("SELECT version();")
+        version = curr.fetchone()    
+        conn.close()
+        return {"Postgress version": version}
+    except Exception as e:
+        return {"status": "Database connection failed", "error": str(e)}
 
 
 # if __name__ == "__main__":
